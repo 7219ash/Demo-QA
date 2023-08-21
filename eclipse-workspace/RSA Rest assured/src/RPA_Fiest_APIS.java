692f9fe96122bdafc54b7cb86ea06d95929249d8
patilashis <113262882+patilashis@users.noreply.github.com>
2023-08-21 16:46:31 +0530

    second commitment

--- eclipse-workspace/RSA Rest assured/src/RPA_Fiest_APIS.java
+++ eclipse-workspace/RSA Rest assured/src/RPA_Fiest_APIS.java
@@ -3,8 +3,14 @@ public class RPA_Fiest_APIS
 {
 	public static void main(String[] args)
 	{
-	System.out.println("hello world");
+	System.out.println("Ashish");
 	System.out.println("the hollywood");
+	System.out.println("improving");
+	
+	for(int i=0; i<5;i++)
+	{
+		System.out.println("rrr= "+ i);
+	}
 	}
 
 	
